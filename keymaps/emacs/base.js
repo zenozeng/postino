06e607e173a87e65548fc68c106fd78df0ade6b4
Zeno Zeng <zenoofzeng@gmail.com>
2016-07-24 18:33:14 +0800

    init keymaps

---
+++ keymaps/emacs/base.js
@@ -0,0 +1,12 @@
+class Base {
+
+}
+
+
+module.exports = {
+    "Ctrl-a": "Home",
+    "Ctrl-x Ctrl-s": "Ctrl-s",
+    "Ctrl-x k": "Ctrl-w",
+    "Ctrl-x Ctrl-c": "Alt-F4",
+    "Ctrl-k": "Shift-End Ctrl-x"
+}
