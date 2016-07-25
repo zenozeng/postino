b55e1f3727d6400f9d112316b6e31919390251b9
Zeno Zeng <zenoofzeng@gmail.com>
2016-07-26 01:27:52 +0800

    keymap.send

--- keymaps/base.js
+++ keymaps/base.js
@@ -1,3 +1,5 @@
+import * as robot from 'robotjs';
+
 class Keymap {
 
     constructor(options) {
@@ -10,6 +12,25 @@ class Keymap {
     }
 
     send(keystroke) {
+        let queue = keystroke.split(' ');
+        while (queue.length > 0) {
+            let keys = queue.shift().toLowerCase().split('+');
+            let key = null;
+            let modifier = [];
+            keys.forEach((k) => {
+                if (k == 'ctrl') {
+                    modifier.push('control');
+                }
+                else if (k == 'shift') {
+                    modifier.push('shift');
+                }
+                else {
+                    key = k;
+                }
+            });
+            console.log({keyTap: {key, modifier}});
+            robot.keyTap(key, modifier);
+        }
         console.log({send: {keystroke}});
     }
 
