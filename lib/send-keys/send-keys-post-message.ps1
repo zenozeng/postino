93a5f21e7617c4726d0bba35736d968a6fa72c5f
Zeno Zeng <zenoofzeng@gmail.com>
2016-07-27 21:45:51 +0800

    Add-Type: AttachThreadInput

--- lib/send-keys/send-keys-post-message.ps1
+++ lib/send-keys/send-keys-post-message.ps1
@@ -7,10 +7,16 @@ Add-Type @"
     [DllImport("user32.dll")]
     public static extern IntPtr GetForegroundWindow();
 
-    [DllImport("user32.dll", SetLastError=true)]
+    [DllImport("user32.dll")]
     public static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint lpdwProcessId);
+
+    [DllImport("user32.dll")]
+    public static extern bool AttachThreadInput(uint idAttach, uint idAttachTo, bool fAttach);
 }
 "@
+
+[void] [System.Reflection.Assembly]::LoadWithPartialName("'System.Windows.Forms")
+
 
 for (;;) {
     try {
@@ -18,6 +24,7 @@ for (;;) {
         $ThreadProcessId = 0
         [User32]::GetWindowThreadProcessId($ActiveHandle, [ref] $ThreadProcessId)
         Write-Host "Process: $ThreadProcessId"
+        [System.Windows.Forms.SendKeys]::SendWait("{Home}")
     } catch {
         Write-Error "Failed to get active Window details. More Info: $_"
     }
