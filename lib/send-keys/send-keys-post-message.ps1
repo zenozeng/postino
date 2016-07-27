1410ab46fa9d39523560c747124108d74676ba67
Zeno Zeng <zenoofzeng@gmail.com>
2016-07-27 22:08:36 +0800

    AttachThreadInput

--- lib/send-keys/send-keys-post-message.ps1
+++ lib/send-keys/send-keys-post-message.ps1
@@ -1,18 +1,23 @@
 [CmdletBinding()]
 Param()
 Add-Type @"
-  using System;
-  using System.Runtime.InteropServices;
-  public class User32 {
-    [DllImport("user32.dll")]
-    public static extern IntPtr GetForegroundWindow();
+    using System;
+    using System.Runtime.InteropServices;
+    public class User32 {
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetForegroundWindow();
 
-    [DllImport("user32.dll")]
-    public static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint lpdwProcessId);
+        [DllImport("user32.dll")]
+        public static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint lpdwProcessId);
 
-    [DllImport("user32.dll")]
-    public static extern bool AttachThreadInput(uint idAttach, uint idAttachTo, bool fAttach);
-}
+        [DllImport("user32.dll")]
+        public static extern bool AttachThreadInput(uint idAttach, uint idAttachTo, bool fAttach);
+    }
+
+    public class Kernel32 {
+        [DllImport("Kernel32.dll")]
+        public static extern uint GetCurrentThreadId();
+    }
 "@
 
 [void] [System.Reflection.Assembly]::LoadWithPartialName("'System.Windows.Forms")
@@ -21,10 +26,13 @@ Add-Type @"
 for (;;) {
     try {
         $ActiveHandle = [User32]::GetForegroundWindow()
-        $ThreadProcessId = 0
-        [User32]::GetWindowThreadProcessId($ActiveHandle, [ref] $ThreadProcessId)
-        Write-Host "Process: $ThreadProcessId"
+        $TargetThreadProcessId = 0
+        [User32]::GetWindowThreadProcessId($ActiveHandle, [ref] $TargetThreadProcessId)
+        $CurrentThreadId = [Kernel32]::GetCurrentThreadId()
+        [User32]::AttachThreadInput($CurrentThreadId, $TargetThreadProcessId, $TRUE); # attach
+        Write-Host "Process: $TargetThreadProcessId"
         [System.Windows.Forms.SendKeys]::SendWait("{Home}")
+        [User32]::AttachThreadInput($CurrentThreadId, $TargetThreadProcessId, $FALSE); # detach
     } catch {
         Write-Error "Failed to get active Window details. More Info: $_"
     }
