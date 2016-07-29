0a9ebea7bb50ae03cb4a1100a5c8ac550df12e34
Zeno Zeng <zenoofzeng@gmail.com>
2016-07-29 21:09:39 +0800

    home, end, up, down, right and left for xdotool

--- keymaps/emacs/default.js
+++ keymaps/emacs/default.js
@@ -7,11 +7,16 @@ class EmacsKeymap extends Keymap {
         let map = {
             'Ctrl+a': 'Home',
             'Ctrl+e': 'End',
+            'Ctrl+f': 'Right',
+            'Ctrl+b': 'Left',
+            'Ctrl+p': 'Up',
+            'Ctrl+n': 'Down',
             'Ctrl+x Ctrl+s': "Ctrl+s",
             'Ctrl+x k': 'Ctrl+w',
             'Ctrl+x Ctrl+c': 'Alt+F4',
             'Ctrl+k': 'Shift+End Ctrl+x',
-            'Ctrl+/': 'Ctrl+z'
+            'Ctrl+/': 'Ctrl+z',
+            'Ctrl+g': 'Esc'
         }
 
         Object.keys(map).forEach((k) => {
