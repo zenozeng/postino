ced3b5fe1796eaf93e574090e14757448dba2419
Zeno Zeng <zenoofzeng@gmail.com>
2016-08-02 22:36:05 +0800

    def is_keyboard_device

--- lib/evdev/main.py
+++ lib/evdev/main.py
@@ -1,13 +1,27 @@
 import asyncio, evdev
 from evdev import UInput, ecodes
 
-# List accessible event devices
-devices = [evdev.InputDevice(fn) for fn in evdev.list_devices()]
-for device in devices:
+def is_keyboard_device(device):
     capabilities = device.capabilities()
     if 1 in capabilities: # EV_KEY
         if ecodes.KEY_SPACE in capabilities[1]:
-            print("Keyboard device found:", device.name, device.phys)
+            return True
+    return False
+
+# List accessible event devices
+devices = [evdev.InputDevice(fn) for fn in evdev.list_devices()]
+keyboardDevices = list(filter(is_keyboard_device, devices))
+print(keyboardDevices)
+
+async def print_events(device):
+    async for event in device.async_read_loop():
+        print(device.fn, evdev.categorize(event), sep=': ')
+
+for device in keyboardDevices:
+    asyncio.ensure_future(print_events(device))
+
+loop = asyncio.get_event_loop()
+loop.run_forever()
 
 # Init uinput device
 uinput = UInput(name='postino-uinput-device')
