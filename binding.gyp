5a6021dc8b04ca16228951122a55bb9a00d0cd70
Zeno Zeng <zenoofzeng@gmail.com>
2016-08-07 16:52:59 +0800

    evdev.cc & libevdev.h

---
+++ binding.gyp
@@ -0,0 +1,8 @@
+{
+    "targets": [
+        {
+            "target_name": "evdev",
+            "sources": [ "src/evdev/evdev.cc" ]
+        }
+    ]
+}
--- src/evdev/evdev.cc
+++ src/evdev/evdev.cc
@@ -1,2 +1,24 @@
-#include <libevdev.h>
+#include "libevdev.h"
 #include <nan.h>
+
+using v8::FunctionTemplate;
+using v8::Handle;
+using v8::Object;
+using v8::String;
+using Nan::GetFunction;
+using Nan::New;
+using Nan::Set;
+
+struct libevdev *dev;
+
+NAN_METHOD(new_from_fd) {
+    int fd = info[0]->Uint32Value();
+    libevdev_new_from_fd(fd, &dev);
+}
+
+NAN_MODULE_INIT(InitAll) {
+    Set(target, New<String>("newFromFd").ToLocalChecked(),
+        GetFunction(New<FunctionTemplate>(new_from_fd)).ToLocalChecked());
+}
+
+NODE_MODULE(addon, InitAll)
